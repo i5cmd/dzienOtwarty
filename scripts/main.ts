e6485ed5a2c9f7040793fefea6ecc8ed7b27b189
i5cmd <dawidt182@gmail.com>
2026-05-10 16:24:01 +0200

    Ulepszenie rejestracji 10.05.2026 16:23

--- scripts/main.ts
+++ scripts/main.ts
@@ -132,6 +132,7 @@ $GALLERY_IMAGE.click(() => galleryManagement.fullscreenMode(true, $GALLERY_IMAGE
 $CLOSE_FLSC.click(() => galleryManagement.fullscreenMode(false, $GALLERY_IMAGE, $FULLSCREEN_IMAGE, $FULLSCREEN_CONTAINER));
 $FULLSCREEN_IMAGE.on("pointerdown", (e: any) => galleryManagement.grabPhoto(true, e, $FULLSCREEN_IMAGE));
 $FULLSCREEN_IMAGE.on("pointerup", (e: any) => galleryManagement.grabPhoto(false, e, $FULLSCREEN_IMAGE));
+$FULLSCREEN_CONTAINER.on("pointerup", (e: any) => galleryManagement.grabPhoto(false, e, $FULLSCREEN_IMAGE));
 $ZOOM_IN_BUTTON.click(() => galleryManagement.zoom(true, $FULLSCREEN_IMAGE));
 $ZOOM_OUT_BUTTON.click(() => galleryManagement.zoom(false, $FULLSCREEN_IMAGE));
 $FULLSCREEN_IMAGE.on("wheel", (e: JQuery.TriggeredEvent) => {
